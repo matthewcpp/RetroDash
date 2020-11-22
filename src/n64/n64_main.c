c427cbce2dce0f5c7096b71bbc0dbe4d4e104b9d
Matthew LaRocca <mlarocca.cpp@gmail.com>
2020-11-21 17:15:01 -0800

    Timing

--- src/n64/n64_main.c
+++ src/n64/n64_main.c
@@ -15,11 +15,7 @@
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 240
 
-static volatile int tick_count = 0;
-
-void timer_tick(int overflow) {
-    tick_count += 1;
-}
+#define UPDATE_MS 16
 
 int main(void)
 {
@@ -30,60 +26,57 @@ int main(void)
     controller_init();
     timer_init();
 
-
     Audio* audio = n64_audio_create();
     Input* input = n64_input_create();
     Renderer* renderer = n64_renderer_create(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     Game* game = game_create(audio, input, renderer);
 
-    new_timer(TIMER_TICKS(1000), TF_CONTINUOUS, timer_tick);
-    //unsigned long prev_time = get_ticks_ms();
+    float seconds = 0;
+    char msg[20];
 
-
-    /* Main loop test */
+    (void)game;
+    unsigned long prev_update_time = get_ticks_ms();
     while(1) 
     {
-        //unsigned long current_time = get_ticks_ms();
-        //float time_delta = (current_time - prev_time) / 1000.0f;
-        //(void)time_delta;
-
         n64_audio_update(audio);
 
-        if (tick_count >= 16) {
-            float time_delta = 60.0f / 1000.0f;
+        unsigned long current_update_time = get_ticks_ms();
+
+        if (current_update_time < prev_update_time) {
+            prev_update_time = current_update_time;
+            continue;
+        }
+
+        unsigned long time_delta_ms =current_update_time - prev_update_time;
+
+        if (time_delta_ms >= UPDATE_MS) {
+            float time_delta = (float)time_delta_ms / 1000.0f;
+            seconds += time_delta;
+
+            n64_audio_update(audio);
             n64_input_update(input);
             game_update(game, time_delta);
 
-            tick_count = 0;
+            prev_update_time = current_update_time;
         }
 
-        /* Grab a render buffer */
         while( !(renderer->display_context = display_lock()) );
-       
-        /*Fill the screen */
+
         graphics_fill_screen( renderer->display_context, renderer->clear_color);
-
-        /* Assure RDP is ready for new commands */
         rdp_sync( SYNC_PIPE );
-
-        /* Remove any clipping windows */
         rdp_set_default_clipping();
-
-        /* Attach RDP to display */
         rdp_attach_display( renderer->display_context );
-
         rdp_sync( SYNC_PIPE );
 
         game_draw(game);
 
-        /* Inform the RDP we are finished drawing and that any pending operations should be flushed */
         rdp_sync( SYNC_PIPE );
         rdp_detach_display();
 
-        /* Force backbuffer flip */
+        sprintf(msg, "seconds: %f", seconds);
+        graphics_set_color( 0xFFFFFFFF, 0x0 );
+        graphics_draw_text( renderer->display_context, 20, 20, msg);
         display_show(renderer->display_context);
-
-        //prev_time = current_time;
     }
 }
